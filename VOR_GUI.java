4d325360e3ff3f9c2a8f1dfa45436ba41a7ea0b2
shreyasingh22 <shreya@mail.fresnostate.edu>
2014-11-20 13:51:01 -0800

    VOR_GUI
    Added new buttons

--- VOR_GUI.java
+++ VOR_GUI.java
@@ -1,20 +1,23 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
-import java.awt.EventQueue;
 
+import java.awt.EventQueue;
 import java.awt.Graphics;
 import java.awt.Color;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JButton;
+import javax.swing.JLabel;
+
 import java.awt.BorderLayout;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-
-
-public class VOR_GUI  extends JFrame
+public class VOR_GUI extends JFrame
 {
+	private ImageIcon image;
+	
 	public VOR_GUI()
 	{
 		setTitle("VOR_GUI");
@@ -22,21 +25,39 @@ public class VOR_GUI  extends JFrame
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setResizable(false);
-		setLayout(null);
+		getContentPane().setLayout(null);
 		
-		JButton btnNewButton = new JButton("New button");
+		
+		
+	JButton btnNewButton = new JButton(new ImageIcon("Images/obs1.gif"));
+	JButton btn1NewButton = new JButton(new ImageIcon("Images/up1.gif"));
+	JButton btn2NewButton = new JButton(new ImageIcon("Images/down1.gif"));
 		//btnNewButton.setSize(20,20);
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
+		//btnNewButton.addActionListener(new ActionListener() 
+		//{
+			//public void actionPerformed(ActionEvent arg0) {
+			//}
+		//});
+		
 		getContentPane().add(btnNewButton);
-		btnNewButton.setBounds(310,195, 30,30);
+		btnNewButton.setBounds(310,200, 30,30);
+		getContentPane().add(btn1NewButton);
+		btn1NewButton.setBounds(418,275,43,50);
+		getContentPane().add(btn2NewButton);
+		btn2NewButton.setBounds(418,325,43,50);
+		
+		
+		JLabel lblNewLabel = new JLabel();
+		getContentPane().add(lblNewLabel);
+		lblNewLabel.setBackground(Color.BLACK);
+		lblNewLabel.setOpaque(true);
+		lblNewLabel.setBounds(410,150,10,10);
+		
+		
 		
 	}
 	public void paint(Graphics g)
 	{
-
 		g.drawLine(300, 0, 300, 480);
 		g.setColor(Color.BLACK);
 		g.drawLine(300, 270, 480, 270);
@@ -44,6 +65,11 @@ public class VOR_GUI  extends JFrame
 	    g.drawOval(315,100,140,140);
 	    g.drawOval(332,117,108,108);
 	    g.drawLine( 332,170,440,170);
+	    g.drawRect(315,300,150,100);
+	    g.drawLine(420,300,420,400);
+	    g.drawLine(420,350,465,350);
+	   g.drawLine(150,140,150,340);
+	   g.drawLine(50,240,250,240);
 	   g.drawRect(345,155,10,10);
 	   g.drawRect(345,175,10,10);
 	   g.drawLine(350,168,350,172);
@@ -54,11 +80,14 @@ public class VOR_GUI  extends JFrame
 	   g.drawLine(400,168,400,172);
 	   g.drawLine(410,168,410,172);
 	   g.drawLine(420,168,420,172);
-	   }
+	  
+	}
 	public static void main(String[] args)
 	{
 		VOR_GUI t = new VOR_GUI();
 		
 	}
 }
+
+
 
